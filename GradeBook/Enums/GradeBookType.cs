1242551450d4ea1fb302e086a3824de78c12aef5
JohanFL <66465924+JohanFL@users.noreply.github.com>
2020-06-18 07:32:25 +0200

    Add files via upload

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public Enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
